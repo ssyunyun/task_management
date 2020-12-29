2eafb603dd465778dcab94a771c212c1fe3e1eab
ssyunyun <1ss.yun025@gmail.com>
2020-12-29 22:09:47 +0900

    sqlite削除

--- android/app/src/main/java/com/task_management/MainApplication.java
+++ android/app/src/main/java/com/task_management/MainApplication.java
@@ -6,7 +6,6 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import io.realm.react.RealmReactPackage;
 import io.realm.react.RealmReactPackage;
-import org.pgsqlite.SQLitePluginPackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
